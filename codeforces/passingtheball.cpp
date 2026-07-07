edd1e8ffdb5ac2dfd11393dd4a24a896fb472534
viksuuu... <147147366+jogivikas@users.noreply.github.com>
2026-07-07 14:19:16 +0530

    more

---
+++ codeforces/passingtheball.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+
+        cin >> s;
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == 'R')
+            {
+                count++;
+            }
+            else
+            {
+                count++;
+                break;
+            }
+        }
+        cout << count << endl;
+    }
+
+    return 0;
+}
