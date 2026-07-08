65281675ab4ca375086d33ef1eb91ea044c1ef42
viksuuu... <147147366+jogivikas@users.noreply.github.com>
2026-07-08 14:47:58 +0530

    more

---
+++ codeforces/aliceandbob.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace  std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+
+int n,a;
+cin>>n>>a;
+vector<int>v(n);
+
+for(int i=0;i<n;i++)
+{
+    cin>>v[i];
+}
+    }
+    return 0;
+}
