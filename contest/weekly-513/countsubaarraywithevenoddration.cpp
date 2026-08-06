89fc45307ab9c504a3059bf7b7cb35b4e1e98f5c
viksuuu... <147147366+jogivikas@users.noreply.github.com>
2026-08-06 10:55:23 +0530

    more

---
+++ contest/weekly-513/countsubaarraywithevenoddration.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int countRatioSubarrays(vector<int> &nums, int a, int b)
+    {
+
+        int n = nums.size();
+        int count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            int even = 0;
+            int odd = 0;
+
+            for (int j = i; j < n; j++)
+            {
+
+                if (nums[j] % 2 == 0)
+                    even++;
+                else
+                    odd++;
+
+                if (odd > 0 && 1LL * even * b <= 1LL * odd * a)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+};
